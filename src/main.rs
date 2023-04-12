9463058988376d83672e77a09a94e7d4e1d450bf
M-Farag <mina.farag@icloud.com>
2023-04-13 01:48:45 +0200

    ADD Text swapping functionality

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    // ToDo
+    // [] Accept arguments
+    // [] Read the file
+    // [] Parse the file & swap the letters
+    // [] Write the file
+
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let origin_letter = &args[2];
+    let new_letter = &args[3];
+
+    let mut contents = fs::read_to_string(file_path).unwrap();
+    let re = Regex::new(&origin_letter).unwrap();
+    contents = re.replace_all(&contents, new_letter).to_string();
+
+    fs::write(file_path, contents).unwrap();
+
+}
